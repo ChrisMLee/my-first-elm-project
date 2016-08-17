ff2c5ad198e6383ff568aea4152a222c5ea32bdc
Christopher Lee <cristovao.m.lee@gmail.com>
2016-08-16 20:39:46 -0400

    dual components working correctly

--- Main.elm
+++ Main.elm
@@ -1,21 +1,61 @@
 module Main exposing (..)
 
 import Html.App exposing (beginnerProgram)
+import Html exposing (Html, Attribute, div, input, text, p, h1)
 import ParserComponent exposing (Model, Msg, init, view, update)
 
 -- Main
 main =
     Html.App.beginnerProgram
-        { model = ParserComponent.init ""
-        , view = ParserComponent.view
-        , update = ParserComponent.update
+        { model = init "" ""
+        , view = view
+        , update = update
         }
 
 -- Model
---type alias Model =
---    { firstParser : ParserComponent.Model
---    , secondParser : ParserComponent.Model
---    }
+type alias Model =
+    { firstParser : ParserComponent.Model
+    , secondParser : ParserComponent.Model
+    }
 
---init first second =
---    Model first second
+
+init : String -> String -> Model
+init first second =
+  { firstParser = ParserComponent.init first
+    , secondParser = ParserComponent.init second
+  }
+
+-- UPDATE
+
+
+type Msg
+    = First ParserComponent.Msg
+    | Second ParserComponent.Msg
+
+
+update : Msg -> Model -> Model
+update msg model =
+ case msg of
+   First m ->
+     { model | firstParser = ParserComponent.update m model.firstParser}
+
+   Second m ->
+     { model | secondParser = ParserComponent.update m model.secondParser}
+
+-- VIEW
+
+view model =
+    div [] [ 
+      div []
+          [ 
+          h1 [] [text "first"]
+          , Html.App.map First
+              (ParserComponent.view model.firstParser)
+          ]
+      , div []
+          [ 
+          h1 [] [text "second"]
+          , Html.App.map Second
+              (ParserComponent.view model.secondParser)
+          ]
+    ]
